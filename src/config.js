11110491592a1b48e7cc17fe2a57d4ac72ab9f4c
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-06-27 19:41:18 -0400

    change API url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  // API_ENDPOINT: 'https://indienexus-api.herokuapp.com/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://jessegilbride-lifeline-api.herokuapp.com/',
   TOKEN_KEY: 'indienexus-client-auth-token',
 }
