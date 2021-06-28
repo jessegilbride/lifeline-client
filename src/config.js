fd2badacfb9ed3ab367ae3072d1b97bc3467877f
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-06-27 20:45:26 -0400

    implement API conditional with .env file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 export default {
   // API_ENDPOINT: 'http://localhost:8000/api',
-  API_ENDPOINT: 'https://jessegilbride-lifeline-api.herokuapp.com/api',
+  // API_ENDPOINT: 'https://jessegilbride-lifeline-api.herokuapp.com/api',
+  API_ENDPOINT: (process.env.NODE_ENV === 'development') ? 'http://localhost:8000/api' : 'https://jessegilbride-lifeline-api.herokuapp.com/api',
   TOKEN_KEY: 'lifeline-client-auth-token',
 }
