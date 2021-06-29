7cabe819651b9168b502806738d24899acb41484
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-06-29 15:46:09 -0400

    make spiffy header

--- src/routes/HomeLanding/HomeLanding.js
+++ src/routes/HomeLanding/HomeLanding.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 // import TimelineContext from '../../contexts/TimelineContext';
 import './HomeLanding.css';
-import timelineGraphic from '../../images/timeline-512x512.png'
+// import timelineGraphic from '../../images/timeline-512x512.png'
 
 export default class HomeLanding extends Component {
   // static contextType = TimelineContext;
@@ -11,15 +11,19 @@ export default class HomeLanding extends Component {
       <Fragment>
         <section className='page-top-banner' id='homepage-banner'>
           <h1 className=''>
-            It's hard to remember everything in life. 
-            <br />
-            <br />
-            So make a timeline!
+            <span>Make a timeline of your life.</span>
+            {/* <span className='bi-line'></span> */}
           </h1>
         </section>
         <section className='site-intro-section'>
-          <img src={timelineGraphic} alt='timeline graphic' className='homepage-timeline-graphic' />
-          <p className='site-intro-content'></p>
+          {/* <img src={timelineGraphic} alt='timeline graphic' className='homepage-timeline-graphic' /> */}
+          <h2 className='site-intro-content'>Get started...</h2>
+          <ol className='site-intro-features'>
+            <li>login (or register)</li>
+            <li>create the timeline</li>
+            <li>add entries to the line</li>
+            <li className='feature-greyed'>(more features coming soon)</li>
+          </ol>
         </section>
 
       </Fragment>
